8b6bb1883929c0e340f4f04ce25662a99a3e0049
LambdaGaming <starwars18m@gmail.com>
2022-01-31 17:53:11 -0500

    Added custom hex input

--- led_web/data/input.js
+++ led_web/data/input.js
@@ -1,6 +1,24 @@
 function SendColor() {
 	var colorstring = document.getElementById( "colorinput" ).value
-	var color = "0x" + colorstring.substring( 1 )
+	var hexstring = document.getElementById( "hexinput" ).value
+	var color
+
+	if ( hexstring.length > 0 ) {
+		if ( hexstring.startsWith( "0x" ) ) {
+			color = hexstring
+		}
+		else if ( hexstring.startsWith( "#" ) ) {
+			color = hexstring.substring( 1 )
+			color = "0x" + color
+		}
+		else {
+			color = "0x" + hexstring
+		}
+	}
+	else {
+		color = "0x" + colorstring.substring( 1 )
+	}
+	
 	var connect = new XMLHttpRequest()
 	connect.open( "GET", "/state?color=" + Number( color ), true )
 	connect.send()
