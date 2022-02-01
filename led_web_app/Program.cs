e94773c62186fd828c040dc59c3c98b56912144a
LambdaGaming <starwars18m@gmail.com>
2022-02-01 18:58:37 -0500

    Color name support

--- led_web_app/Program.cs
+++ led_web_app/Program.cs
@@ -1,24 +1,35 @@
 ﻿// Simple .NET 6 console program to show how the led_web Arduino program can be interacted with outside of a browser
 
-HttpClient client = new HttpClient();
-string input = "";
-bool formatted = false;
+using System.Drawing;
 
-Console.WriteLine( "Enter a hex number:" );
+HttpClient client = new();
+string input;
+bool namedcolor = false;
+
+Console.WriteLine( "Enter a hex number or color name:" );
 while ( ( input = Console.ReadLine() ) != null && input != "exit" )
 {
-	if ( input.StartsWith( "0x" ) )
-		formatted = true;
+	int color = 0;
+	int realcolor = Color.FromName( input ).ToArgb();
+
+	if ( realcolor != 0 )
+	{
+		Console.WriteLine( "Color name detected." );
+		color = realcolor;
+		namedcolor = true;
+	}
 
 	try
 	{
 		Console.WriteLine( "Attempting to parse and apply color..." );
 
-		int color;
-		if ( formatted )
+		if ( !namedcolor )
+		{
+			if ( !input.StartsWith( "0x" ) )
+				input = "0x" + input;
+
 			color = Convert.ToInt32( input, 16 );
-		else
-			color = int.Parse( input );
+		}
 
 		await client.GetAsync( "http://192.168.1.208/state?color=" + color );
 		Console.WriteLine( "Successfully changed the color to " + input + "\n" );
@@ -28,5 +39,5 @@ while ( ( input = Console.ReadLine() ) != null && input != "exit" )
 		Console.WriteLine( "\nAn error occured while attempting to change the color:\n" + e.Message + "\n" );
 	}
 
-	Console.WriteLine( "Enter a hex number:" );
+	Console.WriteLine( "Enter a hex number or color name:" );
 }
