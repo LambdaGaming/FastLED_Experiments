adc9e88c59d15bf034a94a7fd5cfbb7a6854340e
LambdaGaming <starwars18m@gmail.com>
2022-02-25 19:25:42 -0500

    Website improvements

--- led_web/data/input.js
+++ led_web/data/input.js
@@ -1,14 +1,13 @@
-function SendColorSelector() {
-	var colorstring = document.getElementById( "colorinput" ).value
-	var color = "0x" + colorstring.substring( 1 )
-	var connect = new XMLHttpRequest()
-	connect.open( "GET", "/state?color=" + Number( color ), true )
-	connect.send()
+function ValidNumber( num ) {
+	return num >= 0 && num < 200
 }
 
-function SendHexColor() {
+function SendColor() {
+	var colorstring = document.getElementById( "colorinput" ).value
 	var hexstring = document.getElementById( "hexinput" ).value
 	var lednum = document.getElementById( "ledinput" ).value
+	var color = "0x" + colorstring.substring( 1 )
+
 	if ( hexstring.length > 0 ) {
 		if ( hexstring.startsWith( "0x" ) ) {
 			color = hexstring
@@ -21,13 +20,50 @@ function SendHexColor() {
 			color = "0x" + hexstring
 		}
 	}
-	var url
-	if ( lednum == null ) {
-		url = "/state?color=" + Number( color )
+
+	color = Number( color )
+	var url = "/state?"
+	if ( lednum.length > 0 ) {
+		if ( lednum.includes( "," ) ) {
+			var split = lednum.split( "," )
+			split.forEach( e => {
+				if ( !ValidNumber( e ) ) {
+					alert( "Invalid number detected." )
+					return
+				}
+				url += "led" + e + "=" + color + "&"
+			} );
+		}
+		else if ( lednum.includes( ":" ) ) {
+			var split = lednum.split( ":" )
+			if ( split[0] > split[1] ) {
+				alert( "The first number of the range must be smaller than the second number." )
+				return
+			}
+			if ( !ValidNumber( split[0] ) || !ValidNumber( split[1] ) ) {
+				alert( "Invalid number detected." )
+				return
+			}
+			for ( var i = split[0]; i <= split[1]; i++ ) {
+				url += "led" + i + "=" + color + "&"
+			}
+		}
+		else if ( lednum.match( /^[0-9]+$/ ) != null ) {
+			if ( !ValidNumber( lednum ) ) {
+				alert( "Invalid number detected." )
+				return
+			}
+			url += "led" + lednum + "=" + color
+		}
+		else {
+			alert( "Invalid characters detected." )
+			return
+		}
 	}
 	else {
-		url = "/state?led" + lednum + "=" + Number( color )
+		url += "color=" + Number( color )
 	}
+
 	var connect = new XMLHttpRequest()
 	connect.open( "GET", url, true )
 	connect.send()
