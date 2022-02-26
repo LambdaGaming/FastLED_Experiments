ce2ebb227a14728baed313419161a894962875c9
LambdaGaming <starwars18m@gmail.com>
2022-02-25 19:27:44 -0500

    Minor optimization and format fixes

--- led_web_screen/led_web_screen.py
+++ led_web_screen/led_web_screen.py
@@ -10,18 +10,17 @@ HEIGHT = 1080
 
 def GetPixels():
 	params = ""
-	curtime = time.perf_counter()
+	then = time.perf_counter()
 	for i in range( 199 ):
 		color = pixel[random.randrange( 0, WIDTH ), random.randrange( 0, HEIGHT )]
 		finalcolor = "{:02x}{:02x}{:02x}".format( color[0], color[1], color[2] )
-		finalcolor = str( int( finalcolor, 16 ) )
+		finalcolor = int( finalcolor, 16 )
 		params += "led{0}={1}&".format( i, finalcolor )
 	requests.get( "http://192.168.1.208/state?" + params )
-	print( "Applied colors in {0} seconds.".format( time.perf_counter() - curtime ) )
+	now = time.perf_counter()
+	print( "Applied colors in {:.3f} seconds.".format( now - then ) )
 
 while True:
-	print( "Initializing..." )
 	pixel = ImageGrab.grab().load()
-	print( "Ready!" )
 	GetPixels()
 	time.sleep( 10 )
